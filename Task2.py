abdda177945c88480ec13ec090f4fbd31bcd1105
Alchemist <Alchemist1403@yandex.ru>
2024-10-30 11:14:43 +0300

    Изменил регистр букв в названии переменных

--- Task2.py
+++ Task2.py
@@ -9,24 +9,24 @@ dom.normalize() # оптимизирует работу с файлом
 
 elements = dom.getElementsByTagName('Valute')
 
-Charcode_nominal_dict = {} #Пустой словарь 
+charcode_nominal_dict = {} #Пустой словарь 
 
-Charcode = ''
-Nominal = ''
+charcode = ''
+nominal = ''
 
 for node in elements:
     for child in node.childNodes:
         if child.nodeType == 1:         # 1 - элементный узел (содержит подузлы)
             if child.tagName == 'CharCode':
                 if child.firstChild.nodeType == 3: # 3 - узел содержит текст
-                    Charcode = child.firstChild.data
+                    charcode = child.firstChild.data
             if child.tagName == 'Nominal':
                 if child.firstChild.nodeType == 3:
-                    Nominal = int(child.firstChild.data)
+                    nominal = int(child.firstChild.data)
 
-    Charcode_nominal_dict[Charcode] = Nominal
+    charcode_nominal_dict[charcode] = nominal
 
-print(Charcode_nominal_dict)
+print(charcode_nominal_dict)
 
 xml_file.close()
             
